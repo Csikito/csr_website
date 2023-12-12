0e805b2f1cc7ab2d6de016552f9058545bbd572c
Csikós Ronald <csikosronald@gmail.com>
2023-12-12 11:20:20 +0100

    update project section(display projects)

--- src/components/project/ProjectCard.js
+++ src/components/project/ProjectCard.js
@@ -19,7 +19,7 @@ function ProjectCard({ data }) {
         </h3>
       </div>
 
-      <div className="projectCard absolute w-full h-full min-h-[200px] sm:min-h-[301px] -rotate-y-180  backface-hidden group-hover:rotate-y-0 ">
+      <div className="projectCard absolute w-full h-full min-h-[200px] sm:min-h-[301px] -rotate-y-180 backface-hidden group-hover:rotate-y-0 ">
         <div className="relative w-full h-full overflow-hidden">
           <div className="w-full h-full overflow-hidden rounded-lg">
             <picture>
@@ -31,27 +31,31 @@ function ProjectCard({ data }) {
               />
             </picture>
           </div>
-          <div className="absolute top-0 right-0 w-full h-full flex flex-col justify-end gap-2 dark:bg-black dark:bg-opacity-80 bg-bodyColor bg-opacity-60  rounded-lg font-semibold">
-            <div className="flex justify-between items-center pt-2 px-2 sm:px-6">
-              <h3 className="text-base uppercase text-designColor text-center ">
+          <div className="absolute top-0 right-0 w-full h-full flex flex-col justify-end gap-2 dark:bg-black group-hover:dark:bg-opacity-80 dark:bg-opacity-20 bg-bodyColor group-hover:bg-opacity-60 bg-opacity-10 rounded-lg font-semibold delay-500 duration-1000">
+            <div className="relative flex justify-between items-center pt-2 px-2 sm:px-6">
+              <h3 className="text-base uppercase text-designColor text-center opacity-0 group-hover:opacity-100 delay-700 duration-500 ">
                 {data.title}
               </h3>
-              <div className="flex gap-2">
-                <a
-                  href={data.linkWeb}
-                  className="text-lg w-9 h-9 dark:bg-black bg-white rounded-full cursor-pointer flex justify-center items-center text-gray-400 hover:text-designColor duration-300"
-                >
-                  <FaPager />
-                </a>
-                <a
-                  href={data.linkGit}
-                  className="text-lg w-9 h-9 dark:bg-black bg-white rounded-full cursor-pointer flex justify-center items-center text-gray-400 hover:text-designColor duration-300"
-                >
-                  <FaGithub />
-                </a>
+              <div className=" flex gap-1 sm:gap-10">
+                <div className="sm:absolute -top-72 group-hover:-top-28 left-1/3 sm:-translate-x-1/2 delay-700 duration-500">
+                  <a
+                    href={data.linkWeb}
+                    className="text-xl w-9 h-9 sm:text-3xl sm:w-14 sm:h-14 dark:bg-black bg-white rounded-full cursor-pointer flex justify-center items-center text-gray-400 hover:text-designColor opacity-0 group-hover:opacity-100 delay-500 duration-300"
+                  >
+                    <FaPager />
+                  </a>
+                </div>
+                <div className="sm:absolute -top-72 group-hover:-top-28 left-2/3 sm:-translate-x-1/2 delay-1000  duration-500">
+                  <a
+                    href={data.linkGit}
+                    className="text-xl w-9 h-9 sm:text-3xl sm:w-14 sm:h-14 dark:bg-black bg-white rounded-full cursor-pointer flex justify-center items-center text-gray-400 hover:text-designColor opacity-0 group-hover:opacity-100 delay-500 duration-300"
+                  >
+                    <FaGithub />
+                  </a>
+                </div>
               </div>
             </div>
-            <div className="text-sm tracking-wide pb-2 px-2 sm:py-4 sm:px-6 text-gray-50 duration-300">
+            <div className="text-sm tracking-wide pb-2 px-2 sm:py-4 sm:px-6 text-gray-50 opacity-0 group-hover:opacity-100 delay-700 duration-500">
               <p>{data.description}</p>
             </div>
           </div>
